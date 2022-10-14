359c11297a38510763c1049042efa39552ba8faa
GuilhermeAlves-prog <datedevtx@gmail.com>
2022-10-14 13:43:48 -0300

    add ci

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,18 +10,27 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl':32
+      '2xl': 32,
     },
 
     colors: {
-      'gray-900':'#121214',
-      'gray-800':'#202024',
-      'gray-400':'#76767A',
-      'gray-100':'#E1E1E6',
-      'gray-200':'#C4C4CC',
+      transparent: 'transparent',
 
-      'cyan-500':'#81d8f7',
-      'gray-300':'#9be1f8',
+      black: '#000',
+      white: '#FFF',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9BE1FB'
+      },
     },
     extend: {
       fontFamily: {
